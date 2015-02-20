76598a09861d7593d4aeba105c1ec40f41156531
Andrew Lowson <andrew@lowson.co>
2015-02-20 19:26:30 +0000

    Finished chapter 4 of setup

--- fmk_project/fmk/views.py
+++ fmk_project/fmk/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("FMK...Let's do this!<br> <a href='/fmk/about/'>About</a>")
+
+
+def about(request):
+    return HttpResponse("FMK...is a fun game!<br><a href='/fmk/'>Index</a>")
