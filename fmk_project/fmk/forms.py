3b5593a5f2615203ab201841cdc12fdece7deba7
sjgreig27 <scottgreig27@gmail.com>
2015-03-05 23:25:07 +0000

    Up to 8.2.1. - just started on the forms for the models

---
+++ fmk_project/fmk/forms.py
@@ -0,0 +1,47 @@
+from django import forms
+from fmk.models import Celebrity, Category, Game
+
+
+class CelebrityForm (forms.ModelForm):
+    first_name = forms.CharField(max_length=60, help_text="Enter the first name of the celebrity.")
+    last_name = forms.CharField(max_length=60, help_text="Enter the last name of the celebrity.")
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.all(),
+        help_text="Assign a category to the celebrity."
+    )
+
+    class Meta:
+        model = Celebrity
+        fields = ('first_name', 'last_name', 'category', 'picture')
+
+
+class CategoryForm (forms.ModelForm):
+    name = forms.CharField(max_length=60, help_text="Enter the name of the category.")
+    description = forms.CharField(
+        widget=forms.Textarea,
+        max_length=300,
+        help_text="Enter a description of the category."
+    )
+
+    class Meta:
+        model = Category
+        fields = ('name', 'description')
+
+
+class GameForm (forms.ModelForm):
+    celebrity1 = forms.ModelChoiceField(
+        queryset=Category.objects.all(),
+        help_text="Select a celebrity."
+    )
+    celebrity2 = forms.ModelChoiceField(
+        queryset=Category.objects.all(),
+        help_text="Select a celebrity."
+    )
+    celebrity3 = forms.ModelChoiceField(
+        queryset=Category.objects.all(),
+        help_text="Select a celebrity."
+    )
+
+    class Meta:
+        model = Game
+        fields = ('celebrity1', 'celebrity2', 'celebrity3')
--- fmk_project/fmk/views.py
+++ fmk_project/fmk/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from fmk.models import Celebrity
 
 
 def index(request):
+    most_f_list = Celebrity.objects.order_by('-fuck_count')[:5]
+    most_m_list = Celebrity.objects.order_by('-marry_count')[:5]
+    most_k_list = Celebrity.objects.order_by('-kill_count')[:5]
+    context_dict = {
+        'boldmessage': "FMK will be awesome...eventually.",
+        'fuck_list': most_f_list,
+        'marry_list': most_m_list,
+        'kill_list': most_k_list,
+    }
 
-	context_dict = {'boldmessage': "FMK will be awesome...eventually."}
-        return render(request, 'fmk/index.html',context_dict)
+    return render(request, 'fmk/index.html', context_dict)
+
 
 def about(request):
 
