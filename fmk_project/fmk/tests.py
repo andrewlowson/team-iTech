eb04a52c3cfad50b6e02450ce65f7e91afaae246
Mary McDowall <msmcdowall@gmail.com>
2015-03-23 23:11:13 +0000

    test sign up view and random game view

--- fmk_project/fmk/tests.py
+++ fmk_project/fmk/tests.py
@@ -4,42 +4,26 @@ from fmk.models import *
 from fmk.views import *
 from django.core.urlresolvers import reverse
 
-
-class SignUpTest(unittest.TestCase):
-
-
-    def SignUp(self):
-        "I'm not sure what I'm doing"
-
-        self.request = sign_up('andrewlowson', 'andrew@lowson.co', 'password')
-        # self.form = SignUpForm(data=self.request.POST)
-        # self.user = self.form.save()
-        # self.password = self.user.password
-        # self.user.set_password(self.user.password)
-        # self.user.save()
-        # Player.objects.get_or_create(user=self.user)[0]
-        # self.username = self.user.username
-        # registered=True
-        # self.player = authenticate(username=self.username, password=self.password)
-
-
-        self.assertEqual(username = 'andrewlowson')
-
-
-    def SignIn(self):
-
-        self.request = sign_in(self.username, self.password)
-
-        self.assertEqual(HttpResponseRedirect,'/fmk/')
-
-
-
-class OtherSignUp(unittest.TestCase):
-
-    def FailTest(self):
-
-        "HOping this fails"
-
-        self.request = sign_up(123,123,123)
-
-        assert HttpResponseRedirect == '/fmk/'
+class SignUpViewTests(TestCase):
+    
+    def test_player_creation(self):
+        """
+        checks that when a User is created a corresponding Player is greated
+        does this by checking that there is a Player with the same username 
+        that has a valid gamesPlayed count
+        """
+        user = User(username='test', password='pass')
+        user.save()
+        Player.objects.get_or_create(user=user)[0]
+        player = User.objects.get(username='test')
+        self.assertGreaterEqual(Player.objects.get(user=player).gamesPlayed, 0, 'Error GamesPlayed is less than zero')
+        
+class RandomGameViewTests(self):
+    
+    def test_random_game_with_no_celebrities(self):
+        """
+        If there are no celebrities then an appropriate message should be displayed
+        """
+        response = self.client.get(reverse('random_game'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 
